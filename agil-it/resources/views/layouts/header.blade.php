4f8768fcf5fdbbb328d70dea16d46c403a6ced66
naim.seddar <naim_seddar@ens.univ-artois.fr>
2019-04-02 16:42:35 +0200

    tableau de bord chap1

--- agil-it/resources/views/layouts/header.blade.php
+++ agil-it/resources/views/layouts/header.blade.php
@@ -5,18 +5,23 @@
         Univers7
     </a>
     @guest
-    <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">Login</a>
-        </li>
-    </ul>
-        @else
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">Login</a>
+            </li>
+        </ul>
+    @else
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('welcome')}}">Tableau de bord</a>
+            </li>
+        </ul>
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
                 <a class="nav-link" href="{{route('logout')}}"
                    onclick="event.preventDefault();document.getElementById('logout-form').submit()">Déconnexion</a>
             </li>
             <form action="{{route('logout')}}" method="post" style="display: none;" id="logout-form">@csrf</form>
-            @endguest
         </ul>
+    @endguest
 </nav>
