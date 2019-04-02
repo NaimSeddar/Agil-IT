506470d48a6c5bc5335cb5e0073f29967fcf5481
naim.seddar <naim_seddar@ens.univ-artois.fr>
2019-04-02 10:31:21 +0200

    layout v1

---
+++ agil-it/resources/layout/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Agil'IT - IUT de Lens</title>
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+@yield('css')
+</head>
+<body>
+@include('layouts.header')
+<div class="mt-5">
+    <br>
+    @yield('content')
+</div>
+
+@include('layouts.footer')
+
+@yield('javaScript')
+</body>
+</html>
