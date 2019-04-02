81038387e22aaf7da1c79aab3b6094af35248872
DuhAlex <duhemalexandre1@gmail.com>
2019-04-02 19:13:04 +0200

    modif navbar connexion

--- agil-it/resources/views/layouts/app.blade.php
+++ agil-it/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Univers7') }}</title>
+    <title>Agil'IT - IUT de Lens</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
