4971b898be6f61a7e4bbd3465c70a1fa2f4bb96d
Duhem Alexandre <alexandre_duhem@ens.univ-artois.fr>
2019-04-02 11:30:53 +0200

    base de l'auth

--- agil-it/routes/web.php
+++ agil-it/routes/web.php
@@ -11,13 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/formulaire', 'FormulaireController@index')->name('formulaire');
 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
